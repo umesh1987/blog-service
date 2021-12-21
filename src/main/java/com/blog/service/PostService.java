2329f33388f99e3452f2344deb577a054acf3ff9
umesh1987 <59211403+umesh1987@users.noreply.github.com>
2021-12-21 13:03:04 -0600

    implement get all rest api.

--- src/main/java/com/blog/service/PostService.java
+++ src/main/java/com/blog/service/PostService.java
@@ -1,7 +1,11 @@
 package com.blog.service;
 
+import java.util.List;
+
 import com.blog.dto.PostDto;
 
 public interface PostService {
 	PostDto createPost(PostDto postDto);
+	
+	List<PostDto> getAllPosts();
 }
