a128273e9b1bcdd12379a82db8a6143c7cd93b29
umesh1987 <59211403+umesh1987@users.noreply.github.com>
2021-12-24 10:24:06 -0600

    add update post rest api.

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,13 @@ public class PostController {
 	@GetMapping("/getpostbyId")
 	public ResponseEntity<PostDto> getPostById(@RequestBody PostDto postDto) {
 		return ResponseEntity.ok(postService.getPostById(postDto.getId()));
+	}
+	
+	@PutMapping("/updatepost/{id}")
+	public ResponseEntity<PostDto> updatePost(@RequestBody PostDto postDto, @PathVariable(name = "id") long id) {
+		PostDto postResponse = postService.updatePost(postDto, id);
+		return new ResponseEntity<>(postResponse, HttpStatus.OK);
+		
 	}
 
 }
--- src/main/java/com/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/blog/service/impl/PostServiceImpl.java
@@ -56,4 +56,17 @@ public class PostServiceImpl implements PostService {
 		return maptoDto(post);
 	}
 
+	@Override
+	public PostDto updatePost(PostDto postDto, long id) {
+		// get post by id from database before update or edit post.
+		Post post = postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id));
+		
+		post.setTitle(postDto.getTitle());
+		post.setDescription(postDto.getDescription());
+		post.setContent(postDto.getContent());
+		
+		Post updatePost = postRepository.save(post);
+		return maptoDto(updatePost);
+	}
+
 }
