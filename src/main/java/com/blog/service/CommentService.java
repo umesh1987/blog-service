f21cd709929205a871789b8a6f20f82790c5e338
umesh1987 <59211403+umesh1987@users.noreply.github.com>
2021-12-27 09:20:58 -0600

    fixed bean issue.

--- src/main/java/com/blog/service/CommentService.java
+++ src/main/java/com/blog/service/CommentService.java
@@ -1,10 +1,7 @@
 package com.blog.service;
 
-import org.springframework.stereotype.Service;
-
 import com.blog.dto.CommentDto;
 
-@Service
 public interface CommentService {
 	
 	CommentDto createComment(long postId, CommentDto commentDto);
